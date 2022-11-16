eafa4b2624aadc44888e02cea2b6a746d49e20b0
igor <igorqa2022@outlook.com>
2022-11-16 16:42:17 -0300

    AdicionandoEstilo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Avatar from '../img/Avatar.jpg';
+import '../styles/components/sidebar.sass';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
+    <aside id="sidebar">
+      <img src={Avatar} alt='Imagem de perfil' /> //no Src foi importada a nossa variavel avatar que foi importada acima
+      <p className="title">Analista de qualidade</p>
+      <p>redes sociais</p>
+      <p>Informações de contatos</p>
+      <a href="" className="btn">
+        Dowload CV
+      </a>
+    </aside>
+  );
 }
 
 export default Sidebar
