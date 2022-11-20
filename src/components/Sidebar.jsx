e88ed5bcd545e2e546511add9207fe70707f36aa
igor <igorqa2022@outlook.com>
2022-11-20 11:36:33 -0300

    ...

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,15 @@
+import SocialNetworks from './SocialNetworks';
+
 import Avatar from '../img/Avatar.jpg';
+
 import '../styles/components/sidebar.sass';
 
 const Sidebar = () => {
   return (
     <aside id="sidebar">
-      <img src={Avatar} alt='Imagem de perfil' /> //no Src foi importada a nossa variavel avatar que foi importada acima
+      <img src={Avatar} alt='Imagem de perfil' /> //no Src foi usamos a nossa variavel avatar que foi importada acima
       <p className="title">Analista de qualidade</p>
-      <p>redes sociais</p>
+      <SocialNetworks />
       <p>Informações de contatos</p>
       <a href="" className="btn">
         Dowload CV
